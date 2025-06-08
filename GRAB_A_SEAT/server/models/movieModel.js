b4599e348eb293238c52612cc0be4d1bb4d35507
Keerthana L S <keerthanals270405@gmail.com>
2025-06-08 15:25:41 +0530

    movie model update

--- GRAB_A_SEAT/server/models/movieModel.js
+++ GRAB_A_SEAT/server/models/movieModel.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  genre: { type: String, required: true },
-  duration: { type: String, required: true },
+  genre: { type: [String], required: true },
+  duration: { type: Number, required: true }, 
   description: { type: String },
   language: { type: String, required: true },
   releaseDate: { type: Date, required: true },
-  poster: { type: String },
+  rating: { type: String }, 
+  trailerUrl: { type: String }, // ✅ For YouTube embed URL
+  poster: { type: String }, // URL or file path
   createdAt: { type: Date, default: Date.now }
 });
 
